2f460a5362f6b00f19d2c862584c2f1d70678e77
crash2bash <57214337+crash2bash@users.noreply.github.com>
2020-01-18 12:31:58 +0200

    First upload

---
+++ gulpfile.js
@@ -0,0 +1,69 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var browserSync = require('browser-sync').create();
+var svgmin = require('gulp-svgmin');
+var imagemin = require('gulp-imagemin');
+var webp = require('gulp-webp');
+
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+});
+
+gulp.task("watch", function() {
+    browserSync.init({
+        server: "./"
+    });
+    gulp.watch('./sass/**/*.scss', gulp.parallel('sass')).on('change', browserSync.reload);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Минификатор css
+gulp.task('csso', function () {
+    return gulp.src('css/style.css')
+        .pipe(csso())
+        .pipe(gulp.dest('./css/min-style'));
+});
+
+// запуск browserSync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+// оптимизация svg
+gulp.task('svg', function () {
+    return gulp.src('img/**/*.svg')
+        .pipe(svgmin())
+        .pipe(gulp.dest('./optimize-svg'));
+});
+
+// оптимизация img
+gulp.task('img', function() {
+    return gulp.src('img/**/*')
+    .pipe(imagemin([
+        imagemin.mozjpeg({quality: 75, progressive: true}),
+        imagemin.gifsicle({interlaced: true}),
+        imagemin.optipng({optimizationLevel: 5})
+    ]))
+    .pipe(gulp.dest('opt-images'));
+  });
+
+//кодирование jpg в webp
+gulp.task('webp', exports.default = () => (
+	gulp.src('img/**/*.jpg')
+		.pipe(webp({
+            quality: 80,
+            preset: 'photo',
+            method: 6
+        }))
+		.pipe(gulp.dest('opt-webp'))
+));
