a8d4fd44f83d72f5b9314540a893ba03f2b8e4b0
crash2bash <57214337+crash2bash@users.noreply.github.com>
2020-01-30 12:31:52 +0200

    Добавлена кнопка прокрутки вверх страницы

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let mobileMenu = document.querySelector(".main-nav");
 let mobileState = document.querySelector(".site-list__item-wrappper");
 let zeroOpacity = document.querySelector(".site-list");
+let scrollUp = document.querySelector(".scroll__element");
 
 mobileMenu.addEventListener("click", function() {
     if (mobileState.classList.contains("site-list__item--off")) {
@@ -14,3 +15,22 @@ mobileMenu.addEventListener("click", function() {
         zeroOpacity.classList.remove("site-list--color");
     }
 });
+
+// Делаем появление кнопки для прокрутки вверх страницы
+window.onscroll = function() {
+    if (window.pageYOffset > 100) {
+        scrollUp.style.display = "block";
+    }
+    else {
+        scrollUp.style.display = "none";
+    }
+}
+
+// добавляем событие, которое будет следить за кликом по кнопке прокрутки, если клик был, то осуществится плавная прокрутка вверх
+scrollUp.addEventListener("click", function() {
+    window.scrollTo({top: 0, behavior:"smooth"});
+});
+
+if (window.pageYOffset > 100) {
+    scrollUp.classList.add("scroll__element--on");
+}
